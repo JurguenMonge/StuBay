2381117ea3757dac0e02708c353ae5b1afd7cda9
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2023-08-15 12:20:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StuBay</title>
 </head>
+
 <body>
-    
+
     <header>
         <h1>StuBay</h1>
     </header>
 
-    <a href="../StuBay/view/studentView.php">Registro Estudiante</a>
+    <a href="../Stubay/view/studentView.php">Registro Estudiante</a>
     <br>
-    <a href="../StuBay/view/articuloView.php">Registro Articulo</a>
+    <a href="../Stubay/view/articuloView.php">Registro Articulo</a>
+    <br>
+    <a href="../Stubay/view/categoriaView.php">Categorías</a>
 </body>
+
 </html>
