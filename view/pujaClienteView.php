c223bb2fe2861a216f74e1a2987d2d6a045eba7a
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2023-10-09 18:36:55 -0600

    agregando libreria de toats

--- view/pujaClienteView.php
+++ view/pujaClienteView.php
@@ -7,6 +7,8 @@
     <title>Registro de Pujas del Cliente</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 
     <?php
     /*
