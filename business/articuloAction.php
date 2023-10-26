33bae3ef9814a39bd99dd2d126b32c230815fae2
Giancarlo <giancarlo.arias.paisano@est.una.ac.cr>
2023-10-26 17:55:28 -0600

    .

--- business/articuloAction.php
+++ business/articuloAction.php
@@ -4,7 +4,13 @@ include '../business/articuloBusiness.php';
 
 if (isset($_POST['update'])) {
 
-    if (isset($_POST['id']) && isset($_POST['nombre']) && isset($_POST['subcategorias']) && isset($_FILES['articulofotoview'])) {
+    if (
+        isset($_POST['id']) && 
+        isset($_POST['nombre']) && 
+        isset($_POST['subcategorias']) && 
+        isset($_FILES['articulofotoview']) &&
+        isset($_FILES['articulofoto2view'])
+        ) {
 
         $id = $_POST['id'];
         $nombre = $_POST['nombre'];
@@ -15,23 +21,21 @@ if (isset($_POST['update'])) {
         $subcategoria = $_POST['subcategorias'];
         $cliente = $_POST['clienteid'];
         $articuloFoto = $_FILES['articulofotoview'];
+        $articuloFoto2 = $_FILES['articulofoto2view'];
+
+        // Definir la ruta absoluta para el directorio de fotos
+        $directory = "../fotos" . DIRECTORY_SEPARATOR;
 
         // Verifica si se seleccionó una nueva imagen
         if (isset($_FILES['articulofotoview']) && $_FILES['articulofotoview']['error'] === UPLOAD_ERR_OK) {
-            //$directory = "../articulosFotos/";
-            //directory para almacenar las imagenes en la db en linux
-            $directory = realpath("../fotos") . DIRECTORY_SEPARATOR;
             $fileExtension = pathinfo($_FILES['articulofotoview']['name'], PATHINFO_EXTENSION);
             $uniqueFileName = uniqid() . '_' . time() . '.' . $fileExtension;
             $filePath = $directory . $uniqueFileName;
 
-            // Mueve la nueva imagen
-            if (move_uploaded_file($_FILES['articulofotoview']['tmp_name'], $filePath)) {
-                // Imagen cargada con éxito, actualiza el campo de la imagen
-                $articuloFoto = $filePath;
-            }
+            move_uploaded_file($_FILES['articulofotoview']['tmp_name'], $filePath);
+            $articuloFoto = $filePath;
+
         } else {
-            // No se seleccionó una nueva imagen, obtén la imagen actual del artículo
             $articuloBusiness = new ArticuloBusiness();
             $currentArticulo = $articuloBusiness->getArticuloById($id); // Obtener el artículo actual por su ID
             $articuloFoto = $currentArticulo->getArticuloFoto(); // Obtener la imagen actual del artículo actual
@@ -39,22 +43,16 @@ if (isset($_POST['update'])) {
 
         // Verifica si se seleccionó una nueva imagen
         if (isset($_FILES['articulofoto2view']) && $_FILES['articulofoto2view']['error'] === UPLOAD_ERR_OK) {
-            //$directory = "../articulosFotos/";
-            $directory = realpath("../fotos") . DIRECTORY_SEPARATOR;
             $fileExtension = pathinfo($_FILES['articulofoto2view']['name'], PATHINFO_EXTENSION);
             $uniqueFileName = uniqid() . '_' . time() . '.' . $fileExtension;
             $filePath = $directory . $uniqueFileName;
 
-            // Mueve la nueva imagen
-            if (move_uploaded_file($_FILES['articulofoto2view']['tmp_name'], $filePath)) {
-                // Imagen cargada con éxito, actualiza el campo de la imagen
-                $articuloFoto2 = $filePath;
-            }
+            move_uploaded_file($_FILES['articulofoto2view']['tmp_name'], $filePath);
+            $articuloFoto2 = $filePath;
         } else {
-            // No se seleccionó una nueva imagen, obtén la imagen actual del artículo
             $articuloBusiness = new ArticuloBusiness();
             $currentArticulo = $articuloBusiness->getArticuloById($id); // Obtener el artículo actual por su ID
-            $articuloFoto2 = $currentArticulo->getArticuloFoto2(); // Obtener la imagen actual del artículo actual y asignarla a la variable $articuloFoto2 para que no se pierda al actualizar el artículo en la base de datos 
+            $articuloFoto = $currentArticulo->getArticuloFoto(); // Obtener la imagen actual del artículo actual
         }
 
 
--- session/startsession.php
+++ session/startsession.php
@@ -4,7 +4,7 @@ session_start();//start the session
 if(!isset($_SESSION['id'])){//if session not found redirect to login page
 
     session_destroy();//destroy the session
-    header("location: ../view/inicioView.php");//redirect to login page
+    header("location: ../index.php");//redirect to login page
 }
 
 ?>
